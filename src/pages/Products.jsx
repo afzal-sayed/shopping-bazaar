301d444cf4cd66e70b74a76dae36f6e5283215cf
Fauzan <fauzankhot.ft09@gmail.com>
2022-01-23 16:23:44 +0530

    all the images of product have been linked successfully..

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -19,6 +19,18 @@ import foot3 from '../img/footwears/f3.png'
 import foot4 from '../img/footwears/f4.png'
 import foot5 from '../img/women boots.png'
 import foot6 from '../img/footwears/f6.png'
+import watch1 from '../img/watch1.png'
+import mobile from '../img/mobiles and electronics/e1.png'
+import laptop from '../img/mobiles and electronics/e3.png'
+import headphones from '../img/mobiles and electronics/e4.png'
+import tablets  from '../img/mobiles and electronics/e5.png'
+import headphones2 from '../img/mobiles and electronics/e6.png'
+import orange from '../img/fruits & vegetables/v1.png'
+import potato from '../img/fruits & vegetables/v2.png'
+import cabbage from '../img/fruits & vegetables/v3.png'
+import sweet from '../img/fruits & vegetables/v4.png'
+import green from '../img/fruits & vegetables/v5.png'
+import apples from '../img/fruits & vegetables/v6.png'
 export default class Products extends Component {
     render() {
         return (
@@ -384,7 +396,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/watch1.png" alt="" />
+                            <img src={watch1} alt="Watch" />
                             <div className="content">
                                 <h3>Apple Watch Series 3</h3>
                                 <div className="price">Rs.5999.00 <span>Rs.8000.00</span></div>
@@ -402,7 +414,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/mobiles and electronics/e1.png" alt="" />
+                            <img src={mobile} alt="mobile" />
                             &lt;<div className="content">
                                 <h3>One Plus 9 Pro</h3>
                                 <div className="price">Rs. 55999.00 <span>Rs. 59999.00</span></div>
@@ -420,7 +432,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/mobiles and electronics/e3.png" alt="" />
+                            <img src={laptop} alt="laptop" />
                             <div className="content">
                                 <h3>Lenovo Ideapad intel core i5 </h3>
                                 <div className="price">Rs. 32995.00 <span>Rs. 35200.00</span></div>
@@ -438,7 +450,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/mobiles and electronics/e4.png" alt="" />
+                            <img src={headphones} alt="headphones" />
                             <div className="content">
                                 <h3>Sony WH1000XM4 Noice Cancelling Headphones</h3>
                                 <div className="price">Rs. 2999.00<span>Rs. 3200.00</span></div>
@@ -456,7 +468,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/mobiles and electronics/e5.png" alt="" />
+                            <img src={ tablets } alt="mobile" />
                             <div className="content">
                                 <h3>Samsung Galaxy Tab A7 26.31cm 32GB Dark Grey</h3>
                                 <div className="price">Rs. 18999.00 <span>Rs. 20999.00</span></div>
@@ -474,7 +486,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/mobiles and electronics/e6.png" alt="" />
+                            <img src={headphones2} alt="Headphones" />
                             <div className="content">
                                 <h3>Airpods 2nd Generation</h3>
                                 <div className="price">Rs. 15999.00 <span>Rs. 19900.00</span></div>
@@ -502,7 +514,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/fruits & vegetables/v1.png" alt="" />
+                            <img src={orange} alt="friuts" />
                             <div className="content">
                                 <h3>fresh Oranges 1kg</h3>
                                 <div className="price">Rs. 59.00 <span>Rs. 70.00</span></div>
@@ -520,7 +532,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/fruits & vegetables/v2.png" alt="" />
+                            <img src={potato} alt="vegitables" />
                             <div className="content">
                                 <h3>Potatoes 1kg</h3>
                                 <div className="price">Rs. 25.00 <span>Rs. 30.00</span></div>
@@ -538,7 +550,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/fruits & vegetables/v3.png" alt="" />
+                            <img src={cabbage} alt="vegiTable" />
                             <div className="content">
                                 <h3>Fresh Cabbage </h3>
                                 <div className="price">Rs. 49.00 <span>Rs. 55.00</span></div>
@@ -556,7 +568,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/fruits & vegetables/v4.png" alt="" />
+                            <img src={sweet} alt="friut" />
                             <div className="content">
                                 <h3>Sweet Straberries per kg</h3>
                                 <div className="price">Rs. 299.00<span>Rs. 350.00</span></div>
@@ -574,7 +586,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/fruits & vegetables/v5.png" alt="" />
+                            <img src={green} alt="vegiTable" />
                             <div className="content">
                                 <h3>Green Peas 1kg</h3>
                                 <div className="price">Rs. 36.00 <span>Rs. 42.00</span></div>
@@ -592,7 +604,7 @@ export default class Products extends Component {
                             <div className="icons">
                                 <a href="#" className="fas fa-heart" />
                             </div>
-                            <img src="../img/fruits & vegetables/v6.png" alt="" />
+                            <img src={apples} alt="fruits" />
                             <div className="content">
                                 <h3>Fresho Apples 1kg</h3>
                                 <div className="price">Rs. 269.00 <span>Rs. 350.00</span></div>
