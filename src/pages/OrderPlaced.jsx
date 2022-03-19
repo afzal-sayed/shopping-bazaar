83c34314bbed63c16a37e57970859959d679932f
Afzal Sayyed <sayyed0786afzal@gmail.com>
2022-03-19 12:46:32 +0530

    orderPlaced css done

--- src/pages/OrderPlaced.jsx
+++ src/pages/OrderPlaced.jsx
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 export default class OrderPlaced extends Component {
     render() {
         return (
-            <div>
-                
+            <div id="orderContainer">
+                <div id="check"><i className="fas fa-check-circle" /></div>
+                <div id="aboutCheck">
+                    <h1> Order Placed Successfully! </h1>
+                    <p> We've sent you an email with the Order details. </p>
+                </div>
             </div>
         )
     }
